ec4272e919526fb3c35ca35c19f00ca45f392aa7
WANG Haojiao cmcc <wanghaojiao@cmhi.chinamobile.com>
2017-06-30 15:42:58 +0800

    no message

--- TransparentNavibar/DFTransparentNavibar/UIViewController+TransparentNavibar.h
+++ TransparentNavibar/DFTransparentNavibar/UIViewController+TransparentNavibar.h
@@ -12,15 +12,15 @@
 //navibar alpha
 @property (nonatomic) CGFloat navibarAlpha;
 
-//fake navi bar
-@property (nonatomic,strong,readonly) UIImageView* fakeNavigationBar;
-@property (nonatomic) BOOL needsFakeNavibar;
-- (void)createFakeNaviBarOnTop:(BOOL)onTop;
-
-//you should override these method to customize navigation bar appearance for each specific view controller
+//you can override these method to customize navigation bar appearance for each specific view controller
 - (UIImage*)tnw_customizeNavibarBGImage;
 - (UIColor*)tnw_customizeNavibarBGColor;
 - (UIColor*)tnw_customizeNavibarTintColor;
 - (UIFont*)tnw_customizeNavibarTitleFont;
+
+//fake navi bar
+@property (nonatomic,strong,readonly) UIImageView* fakeNavigationBar;
+@property (nonatomic) BOOL needsFakeNavibar;
+- (void)createFakeNaviBarOnTop:(BOOL)onTop;
 
 @end
